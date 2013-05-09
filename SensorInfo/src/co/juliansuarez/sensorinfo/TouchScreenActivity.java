732e85569e3b54ce3dbd5a1393a48d674359b7ab
Julian Suarez <julian.suarez@sooteck.com>
2013-05-09 15:27:38 -0500

    Implementación pantallas de posición y pantalla táctil

--- SensorInfo/src/co/juliansuarez/sensorinfo/TouchScreenActivity.java
+++ SensorInfo/src/co/juliansuarez/sensorinfo/TouchScreenActivity.java
@@ -1,15 +1,29 @@
 package co.juliansuarez.sensorinfo;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.TextView;
 
-public class TouchScreenActivity extends Activity {
+public class TouchScreenActivity extends Activity  implements OnTouchListener{
+	
+	private View viewTouchArea;
+	private TextView textViewXValue;
+	private TextView textViewYValue;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_touch_screen);
+		
+		viewTouchArea = findViewById(R.id.viewTouchArea);
+		textViewXValue = (TextView) findViewById(R.id.textViewXValue);
+		textViewYValue = (TextView) findViewById(R.id.textViewYValue);
+		
+		viewTouchArea.setOnTouchListener(this);
 	}
 
 	@Override
@@ -17,6 +31,15 @@ public class TouchScreenActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.touch_screen, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		synchronized (event) {
+			textViewXValue.setText(Float.toString(event.getX()));
+			textViewYValue.setText(Float.toString(event.getY()));
+		}
+		return false;
 	}
 
 }
